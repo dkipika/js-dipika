be50b3b3f348d5b101e6f03f1a201fe92938e2a6
Dipika01 <126474298+dkipika@users.noreply.github.com>
2024-05-01 05:36:39 +0000

    conversionOfDatatype

---
+++ 01_basic/03_conversionOperation.js
@@ -0,0 +1,25 @@
+let score = "Dipika"
+
+//console.log(typeof score);
+
+let valueInNumber = Number(score);
+
+//console.log(typeof valueInNumber);
+//console.log(valueInNumber);
+
+// "33" => 33
+// "33aa" => NaN
+// true => 1 ; false =>0
+
+let isLoggedI = "dipika"
+
+let booleanIsLoggedIn = Boolean(isLoggedI)
+//console.log(booleanIsLoggedIn);
+
+// 1 => true; 0 => false
+// "" => false
+// "dipika" =>true
+
+let someNum = 33
+let stringNumber = String(someNum);
+console.log(typeof stringNumber);
